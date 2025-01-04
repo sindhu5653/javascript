6aa537f35ced0c6466d794d0a110680387736a53
Sindhu5653 <sindhupvly@gmail.com>
2025-01-04 08:58:11 +0530

    js

---
+++ conditional.js
@@ -0,0 +1,15 @@
+age=19
+vote=(age>=18)?'yes' : 'no'
+console.log(vote)
+
+age=15
+vote=(age>=18)?'yes' : 'no'
+console.log(vote)
+
+num=5
+number=(num%2==0)?'even':'odd'
+console.log(number)
+
+num=10
+number=(num%2==0)?'even':'odd'
+console.log(number)
---
+++ unary.js
@@ -0,0 +1,17 @@
+a='6'
+console.log(a)
+
+console.log(-a)
+
+console.log(typeof(a))
+
+b=10
+console.log(typeof(b))
+
+b='10'-+5
+console.log(b)
+console.log(typeof(b))
+
+b='10'+5
+console.log(b)
+console.log(typeof(b))
