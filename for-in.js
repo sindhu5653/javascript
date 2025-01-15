c78590b93712ae97dd80284f9bc8af0977aba14a
Sindhu5653 <sindhupvly@gmail.com>
2025-01-15 08:53:35 +0530

    java

---
+++ for-in.js
@@ -0,0 +1,11 @@
+// arr=['Apple','Orange','Grape']
+// for(index in arr)
+// {
+//     console.log(arr[index])
+// }
+
+arr=['Apple','Orange','Grape']
+for(index in arr)
+{
+    console.log(index+':'+arr[index])
+}
---
+++ for-of.js
@@ -0,0 +1,5 @@
+arr=['Apple','Orange','Grape']
+for(i of arr)
+{
+    console.log(i)
+}
