b5b57baf1dab143d755811c49cd0f50a2a392e31
Sinshu5653 <sindhupvly@gmail.com>
2025-01-16 23:56:03 +0530

    java

--- calcfn.js
+++ calcfn.js
@@ -1,7 +1,45 @@
-a=50
-b=30
-operator=add
-if(operator='+'){
-    s=add(a,b)
-    console.log(s)
+let num1=prompt('Enter the first number:')
+let operator=prompt('Choose an operation (+, -, *, /):')
+let num2=prompt('Enter the second number:')
+let result
+
+switch (operator) {
+  case "+":
+    res=sum(num1,num2)
+    break;
+  case "-":
+    res=subtract(num1,num2)
+    break;
+  case "*":
+    res=multiply(num1,num2)
+    break;
+  case "/":
+    res=division(num1,num2)
+    break;
+  default:
+    result = "Error: Invalid operator.";
 }
+console.log(res)
+
+
+
+function sum(num1,num2){
+    result=num1+num2;
+    return result;
+}
+
+function subtract(num1,num2){
+    result=num1-num2;
+    return result;
+}
+
+function multiply(num1,num2){
+    result=num1*num2;
+    return result;
+}
+
+function division(num1,num2){
+    result=num1/num2;
+    return result;
+}
+    
