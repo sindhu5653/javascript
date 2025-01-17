4eef544821c2efdaeb765972989ead43b37e8a70
Sinshu5653 <sindhupvly@gmail.com>
2025-01-17 23:15:38 +0530

    java

--- atm.js
+++ atm.js
@@ -2,12 +2,39 @@ var amount=0
 function balance(){
     alert(`Your balance is ${amount}`)
 }
-function deposit(cash){
+function Deposite(cash){
     amount+=cash
-    alert(`Amount credited`)
+    alert(`Amount Credited`)
 }
-function deposit(cash){
-    amount-=cash
-    alert(`Amount debited`)
+function Withdraw(cash){
+    if((amount-cash)<0){
+        alert('Not enough money in the account')
+    }
+    else{
+        amount-=cash
+    alert(`Amount Debited`)
+    }
+}
+while(true){
+    choice=parseInt(prompt('Enter your choice \n 1.Balance \n 2.Deposite \n 3.Withdraw \n 4.Exit'))
+
+    if(choice==1){
+        balance()
+    }
+    else if(choice==2){
+        let cash=parseInt(prompt('Enter the amount that you want to deposite'))
+        Deposite(cash)
+    }
+    else if(choice==3){
+        let cash=parseInt(prompt('Enter the amount that you want to withdraw'))
+        Withdraw(cash)
+    }
+    else if(choice==4){
+        alert(`Account exited successfully`)
+        break
+    }
+    else{
+        alert('Invalid choice')
+    }
 }
 
