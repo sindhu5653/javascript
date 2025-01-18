7b80c8c01fedea20cca8d320633a2d5d32f4cfe9
Sindhu5653 <sindhupvly@gmail.com>
2025-01-18 08:58:15 +0530

    java

---
+++ anonymous.js
@@ -0,0 +1,14 @@
+const s=function(a,b){
+    console.log(a+b)
+}
+s(5,3)
+
+// const a=function(x){
+//     if(x%2==0){
+//         console.log('even')
+//     }
+//     else{
+//         console.log('odd')
+//     }
+// }
+// a(13)
---
+++ arrowfn.js
@@ -0,0 +1,24 @@
+// const add=a=>console.log(a)
+//     res=add(1,2)
+
+const num=a=>{
+if(a%2==0){
+    console.log('even')
+}
+else{
+    console.log('odd')
+}
+}
+num(11)
+
+//    FUNCTION(Ordinary)
+//  ----------------------
+// function num(a){
+//     if(a%2==0){
+//         console.log('even')
+//     }
+//     else{
+//         console.log('odd')
+//     }
+//     }
+// num(16)
