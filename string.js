0a490faa6a344e455b0423645d2ddb05ac3943c5
Sindhu5653 <sindhupvly@gmail.com>
2025-01-24 08:58:08 +0530

    java

---
+++ string.js
@@ -0,0 +1,30 @@
+// string='welcome'
+// console.log(string.length)
+
+// // console.log(string.slice(0,3))
+// console.log(string.slice(3))
+// console.log(string.slice(-4))
+
+// let newstr=string.substr(4)
+// let newstr=string.substr(2,5)
+// console.log(newstr)
+
+// let newstr=string.substring(5)
+// let newstr=string.substring(2,6)
+// console.log(newstr)
+
+// let string='Geeks for Geeks'
+// let newString=string.replace('for','with')
+// console.log(newString)
+
+// let string='Geeks for Geeks for'
+// let newString=string.replaceAll('for','with')
+// console.log(newString)
+
+// let string='Geeks for Geeks'
+// // console.log(string.toUpperCase())
+// console.log(string.toLowerCase())
+
+let str='Synnefo '
+let str2='is the parent company of Novavi'
+console.log(str.concat(str2))
