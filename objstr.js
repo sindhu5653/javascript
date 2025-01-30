ea51ae1b277d80957990d0aadc9691c2865fb4bd
sharonshiju5 <sharonshiju261@gmail.com>
2025-01-30 09:02:49 +0530

    java

---
+++ objstr.js
@@ -0,0 +1,17 @@
+// let obj={name:'Manu',age:20}
+// console.log(obj)
+
+// let objStr=JSON.stringify(obj)
+// console.log(objStr)
+
+// let str_obj=JSON.parse(objStr)
+// console.log(str_obj)
+
+let obj={name:'Manu',age:20}
+console.log(obj)
+
+let objStr=JSON.stringify(obj)
+console.log(objStr)
+
+let str_obj=JSON.parse(objStr)
+console.log(str_obj)
---
+++ value.js
@@ -0,0 +1,13 @@
+// let a=10
+// let b=a
+// b+=1
+// console.log(a,)
+// console.log(b)
+
+function change(x){
+    x+=1
+    console.log(x)
+}
+let a=10
+change(a)
+console.log(a)
