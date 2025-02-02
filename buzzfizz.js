0f04cba572e3d0a64d17137d469a072a5fbccddb
Sinshu5653 <sindhupvly@gmail.com>
2025-02-02 23:56:29 +0530

    java

---
+++ buzzfizz.js
@@ -0,0 +1,4 @@
+// num=prompt('Enter the Number')
+for(i=1;i<=20;i++){
+    console.log(i)
+}
---
+++ eight.js
@@ -0,0 +1,4 @@
+num=prompt('Enter the number')
+for(i=1;i<=10;i++){   
+    console.log(`${i}x${num}=${i*num} \n`)
+}
