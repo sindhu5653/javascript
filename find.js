46373f35fc1543612bbb35f98ef52bce4f1e218e
aleenakrishna <aleenakrish59@gmail.com>
2025-02-22 09:01:36 +0530

    java

---
+++ find.js
@@ -0,0 +1,33 @@
+// let arr=[1,2,3,4]
+// let arr=[5,2,3,4]
+// let arr=[3,2,1,4]
+// let element=arr.find(value=>value<4)
+// console.log(element);
+
+
+// let arr=[{id:1,name:'Jhon'},
+//     {id:2,name:'Alice'},
+//     {id:3,name:'Gokul'}]
+// let element=arr.find(value=>value.id==5)
+//     if(element){
+//         console.log(element)
+//     }
+//     else{
+//         console.log('user not found')
+//     }
+
+// console.log(element);
+
+a=10
+b=2
+c=11
+if(a){
+    if(b){
+        console.log(b)
+    }
+    else{
+        console.log(c)
+    }
+}
+console.log(a)
+   
---
+++ map.js
@@ -0,0 +1,9 @@
+// let arr=[1,2,3,4,5]
+// let sqrArr=arr.map(value=>value*value)
+// {
+// console.log(sqrArr)
+// }
+
+let arr=['apple','orange','grape']
+let newArr=arr.map(value=>value.toUpperCase())
+console.log(newArr)
