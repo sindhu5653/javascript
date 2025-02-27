5bfc51c5861dacf643816156936a16365a66a15e
Sindhu5653 <sindhupvly@gmail.com>
2025-02-27 06:53:36 +0530

    js

--- clock.js
+++ clock.js
@@ -1,5 +1,12 @@
-function time() {
-    let time = new Date();
-    document.getElementById("hr").textContent = time.toLocaleTimeString()
-}
-setInterval(time, 1000);
+// function time() {
+//     let time = new Date();
+//     document.getElementById("hr").textContent = time.toLocaleTimeString()
+// }
+// setInterval(time, 1000);
+
+setInterval(()=>{
+    const today=new Date();
+    document.querySelector("#hr").innerHTML=today.getHours();
+    document.querySelector("#min").innerHTML=today.getMinutes();
+    document.querySelector("#sec").innerHTML=today.getSeconds();
+}) 
