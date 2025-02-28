8f67ac93a794db17c7dbbfb4806100bacc1231fe
Sindhu5653 <sindhupvly@gmail.com>
2025-02-28 09:18:50 +0530

    js

---
+++ exam.js
@@ -0,0 +1,28 @@
+// let arr=[3,4,5,6,7]
+// for(i=3;i<=7;i++){
+//     arr1=[`${i*i}`]
+//     console.log(arr1)
+// }
+
+// let number=prompt('Enter the number')
+// let num=a=>{
+//     if(num%2==0){
+//         console.log('The number is even');
+//     }
+// }
+// num();
+
+
+star='' 
+for (i=1;i<=4;i++){
+    for(j=1;j<=4;j++){
+        star=star+"*"
+    }
+    star=star+'\n'
+}
+console.log(star)
+
+
+// function add(){
+
+// }
